ecdd9ac1d516353dd0261b55981320c2fe3faaa1
Aditya Rishav <52577769+adityarishav@users.noreply.github.com>
2026-01-28 00:52:50 +0530

    Added 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,8 @@ import AdminDashboard from './pages/AdminDashboard';
 import AddMachine from './pages/AddMachine';
 import AnalyticsDashboard from './pages/AnalyticsDashboard';
 import EditMachine from './pages/EditMachine';
-import EditChallenge from './pages/EditChallenge'; // Import EditChallenge
-import AdminChallenges from './pages/AdminChallenges'; // Import AdminChallenges
+import EditChallenge from './pages/EditChallenge'; 
+import AdminChallenges from './pages/AdminChallenges'; 
 import { NotificationProvider } from './components/Notification';
 import MainLayout from './layouts/MainLayout';
 import { AuthProvider } from './contexts/AuthContext';
@@ -22,12 +22,12 @@ import UserManagementPage from './pages/UserManagementPage';
 import ChallengesPage from './pages/ChallengesPage';
 import ChallengeDetail from './pages/ChallengeDetail';
 import AddChallengePage from './pages/AddChallengePage';
-import ProfilePage from './pages/ProfilePage'; // Import ProfilePage
+import ProfilePage from './pages/ProfilePage'; 
 
 import MachinesPage from './pages/MachinesPage';
 
-import AdminBadges from './pages/AdminBadges'; // Import AdminBadges
-import AdminAnnouncements from './pages/AdminAnnouncements'; // Import AdminAnnouncements
+import AdminBadges from './pages/AdminBadges'; 
+import AdminAnnouncements from './pages/AdminAnnouncements'; 
 import SettingsPage from './pages/SettingsPage';
 import VPNSettingsPage from './pages/VPNSettingsPage';
 
@@ -36,6 +36,8 @@ import AcademyLanding from './academy/AcademyLanding';
 import ModuleView from './academy/ModuleView';
 import LessonView from './academy/LessonView';
 import InstructorDashboard from './academy/InstructorDashboard';
+
+import NotFound from './pages/NotFound'; 
 
 function App() {
   return (
@@ -54,7 +56,7 @@ function App() {
                 <Route path="/machines/:machineId" element={<MachineDetail />} />
                 <Route path="/challenges" element={<ChallengesPage />} />
                 <Route path="/challenges/:challengeId" element={<ChallengeDetail />} />
-                <Route path="/profile" element={<ProfilePage />} /> {/* Add Profile Route */}
+                <Route path="/profile" element={<ProfilePage />} />
               </Route>
               <Route element={<ProtectedRoute allowedRoles={['admin']} />}>
                 <Route path="/admin" element={<AdminDashboard />} />
@@ -62,15 +64,13 @@ function App() {
                 <Route path="/admin/analytics" element={<AnalyticsDashboard />} />
                 <Route path="/admin/edit-machine/:machineId" element={<EditMachine />} />
                 <Route path="/admin/users" element={<UserManagementPage />} />
-                <Route path="/admin/challenges" element={<AdminChallenges />} /> {/* New Admin Challenges Route */}
+                <Route path="/admin/challenges" element={<AdminChallenges />} />
                 <Route path="/admin/challenges/new" element={<AddChallengePage />} />
                 <Route path="/admin/edit-challenge/:challengeId" element={<EditChallenge />} />
-                <Route path="/admin/badges" element={<AdminBadges />} /> {/* Add Admin Badges Route */}
-                <Route path="/admin/badges" element={<AdminBadges />} /> {/* Add Admin Badges Route */}
-                <Route path="/admin/announcements" element={<AdminAnnouncements />} /> {/* Add Admin Announcements Route */}
+                <Route path="/admin/badges" element={<AdminBadges />} />
+                <Route path="/admin/announcements" element={<AdminAnnouncements />} />
               </Route>
               <Route path="/settings" element={<SettingsPage />} />
-              <Route path="/vpn" element={<VPNSettingsPage />} />
               <Route path="/vpn" element={<VPNSettingsPage />} />
             </Route>
 
@@ -83,7 +83,7 @@ function App() {
                 <Route path="/academy/lesson/:lessonId" element={<LessonView />} />
               </Route>
             </Route>
-            <Route path="*" element={<Navigate to="/login" replace />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </AuthProvider>
       </NotificationProvider>
